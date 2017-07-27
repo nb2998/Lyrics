951ea3ebe3ef61f3544bf0af43c9d4dded8038b5
Nishtha Bhatia <nbhatia329@gmail.com>
2017-07-27 15:17:13 +0530

    Permission to access storage

--- app/src/main/java/com/apps/nishtha/lyrics/Fragments/AllTracksFragment.java
+++ app/src/main/java/com/apps/nishtha/lyrics/Fragments/AllTracksFragment.java
@@ -1,20 +1,25 @@
 package com.apps.nishtha.lyrics.Fragments;
 
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.apps.nishtha.lyrics.Adapter.TrackAdapter;
 import com.apps.nishtha.lyrics.PojoForId.Track;
 import com.apps.nishtha.lyrics.R;
-import com.apps.nishtha.lyrics.Adapter.TrackAdapter;
 
 import java.util.ArrayList;
 
@@ -25,13 +30,13 @@ import okhttp3.OkHttpClient;
  */
 public class AllTracksFragment extends Fragment {
 
-    String thisTitle,thisArtist;
-    ArrayList<Track> trackArrayList=new ArrayList<>();
+    String thisTitle, thisArtist;
+    ArrayList<Track> trackArrayList = new ArrayList<>();
     RecyclerView recViewForTracksInStorage;
     TrackAdapter trackAdapter;
 
-    StringBuilder trackName,artistName;
-
+    StringBuilder trackName, artistName;
+    public static final int RCODE = 123;
     OkHttpClient okHttpClient;
 
     public AllTracksFragment() {
@@ -41,20 +46,44 @@ public class AllTracksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.all_tracks_fragment,container,false);
+        View view = inflater.inflate(R.layout.all_tracks_fragment, container, false);
 
-        okHttpClient=new OkHttpClient();
+        okHttpClient = new OkHttpClient();
 
-        recViewForTracksInStorage= (RecyclerView) view.findViewById(R.id.recViewForTracksInStorage);
+        recViewForTracksInStorage = (RecyclerView) view.findViewById(R.id.recViewForTracksInStorage);
         recViewForTracksInStorage.setLayoutManager(new LinearLayoutManager(getContext()));
-        trackAdapter=new TrackAdapter(getContext(),trackArrayList);
+        trackAdapter = new TrackAdapter(getContext(), trackArrayList);
         recViewForTracksInStorage.setAdapter(trackAdapter);
 
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, RCODE);
+
+        } else {
+            getMusic();
+        }
+        return view;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == RCODE) {
+            if (permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getMusic();
+            } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(getContext(), "Sorry, permission to access your music files has been denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void getMusic() {
         ContentResolver musicResolver = getContext().getContentResolver();
         Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
 
-        if(musicCursor!=null && musicCursor.moveToFirst()){
+        if (musicCursor != null && musicCursor.moveToFirst()) {
             int titleColumn = musicCursor.getColumnIndex
                     (android.provider.MediaStore.Audio.Media.TITLE);
             int artistColumn = musicCursor.getColumnIndex
@@ -112,12 +141,10 @@ public class AllTracksFragment extends Fragment {
                         }
                     }
                 }
-                trackArrayList.add(new Track(trackName.toString(),artistName.toString(),""));
+                trackArrayList.add(new Track(trackName.toString(), artistName.toString(), ""));
                 trackAdapter.notifyDataSetChanged();
             }
             while (musicCursor.moveToNext());
         }
-
-        return view;
     }
 }
